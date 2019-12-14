0182014b6e421081b15b568038fe8372a742d7cf
admin <2210095180@qq.com>
2019-12-14 21:11:22 +0800

    把本地工程添加到git

---
+++ git-test/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.printf("hello git");
+        System.out.printf("hello git");
+        System.out.printf("hello git");
+        System.out.printf("hello git");
+    }
+}
